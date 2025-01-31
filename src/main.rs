fe5194fe081f6142da85cf0adbfa177388f79f8c
htanmo <145841395+htanmo@users.noreply.github.com>
2025-02-01 02:58:20 +0530

    feat: postgres database added to the project

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use std::env;
+
 use axum::{routing::get, Router};
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+    dotenv().ok();
+
+    // database address
+    let db_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL environment variable is required but not set");
+
+    // database config
+    let db = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&db_url)
+        .await
+        .expect("failed to connect to database");
+
+    // app routes
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     // run our app with hyper, listening globally on port 3000
